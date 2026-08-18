4d7927d13780410c44fce758eb68551738986184
Omid Bolandy <omidbolandy@gmail.com>
2026-08-18 19:02:42 +0330

    Fix asset paths and Cloudflare redirects configuration

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
     plugins: [vue()],
+    base: '/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
 })
-
